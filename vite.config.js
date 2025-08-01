8a5cb85f4e595e4be0f28a00c26976ea14fe29ec
yangjs6 <276993857@qq.com>
2025-08-01 18:31:28 +0800

    将 mapbox-gl 和 three 在打包时做为 external

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,30 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   plugins: [
     dts({
-      include: ['src/**/*'],          // 指定需要生成 d.ts 的源文件
-      outDir: 'dist',               // 输出目录（默认与 build.outDir 一致）
-      insertTypesEntry: true,       // 是否生成统一的 index.d.ts 入口
-      rollupTypes: true             // 是否将所有类型合并成一个文件（推荐开启）
+      include: ['src/**/*'],
+      outDir: 'dist',
+      insertTypesEntry: true,
+      rollupTypes: true
     })
   ],
   build: {
     lib: {
       entry: './src/index.ts',
       name: 'mapbox-3d-tiles',
-      formats: ['es', 'umd'], // 必须指定
+      formats: ['es', 'umd'],
       fileName: (format) =>
         format === 'es'
-          ? 'mapbox-3d-tiles.mjs'   // ← 指定 .mjs
+          ? 'mapbox-3d-tiles.mjs'
           : 'mapbox-3d-tiles.umd.js'
+    },
+    rollupOptions: {
+      external: ['mapbox-gl','three'],
+      output: {
+        globals: {
+          'mapbox-gl': 'mapboxgl',
+          'three': 'THREE'
+        }
+      }
     }
   },
   resolve: {
